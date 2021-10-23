10ac8cd1487d35a57762d8772087ceca7c3b105d
ramiibm <rami.ben-maachia@outlook.fr>
2021-10-23 16:00:46 +0200

    remove unused variable

--- server/src/config/connect.js
+++ server/src/config/connect.js
@@ -2,7 +2,6 @@ const { MongoClient } = require('mongodb');
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@travel-map.vot2f.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
-let mongodb;
 
 const setupDB = async function () {
   try {
