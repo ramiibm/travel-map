ead3ca712143131d8ebdd7a95700814067de477a
ramiibm <rami.ben-maachia@outlook.fr>
2021-10-23 15:17:18 +0200

    add async/await to db connection

--- server/src/config/connect.js
+++ server/src/config/connect.js
@@ -2,16 +2,20 @@ const { MongoClient } = require('mongodb');
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@travel-map.vot2f.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
+let mongodb;
 
-async function run() {
+const setupDB = async function () {
   try {
     await client.connect();
     console.log('Connected correctly to server');
+    mongodb = client.db(process.env.DB_NAME);
   } catch (err) {
     console.log(err.stack);
   } finally {
     await client.close();
   }
-}
+};
 
-run().catch(console.dir);
+const getDB = () => mongodb;
+
+module.exports = { setupDB, getDB };
--- server/src/index.js
+++ server/src/index.js
@@ -3,17 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const { setupDB } = require('./config/connect');
 
+//GraphQL components
 const schema = buildSchema(`
     type Query {
         hello: String
     }
 `);
-
 const root = {
   hello: () => 'Hello World',
 };
 
+// express server
 const app = express();
 app.use(
   '/graphql',
@@ -23,5 +25,14 @@ app.use(
     graphiql: true,
   })
 );
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+const init = async () => {
+  // connect to mongodb
+  await setupDB().catch(console.dir);
+
+  // launch server
+  app.listen(4000);
+  console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+};
+
+init();
